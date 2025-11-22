ec80af267f80f1c2ad361f7872d322a0920986c7
snezhka003 <you@example.com>
2025-11-22 17:32:16 +0300

    add junit4-platform branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,9 +2,11 @@ package ru.netology.service;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
 
 public class CashbackHackServiceTest {
-
+// тесты JUnit4
     @Test
     public void shouldBeRemainIs10() {
         CashbackHackService service = new CashbackHackService();
@@ -33,5 +35,35 @@ public class CashbackHackServiceTest {
         int expected = 900;
 
         assertEquals(expected, actual);
+    }
+// тесты JUnit Jupiter
+@Test
+public void shouldBeRemainIs10JUnitJupiter() {
+    CashbackHackService service = new CashbackHackService();
+
+    int actual = service.remain(990);
+    int expected = 10;
+
+    Assertions.assertEquals(expected, actual);
+}
+
+    @Test
+    public void shouldBeRemainIs0JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBeRemainIs900JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1100);
+        int expected = 900;
+
+        Assertions.assertEquals(expected, actual);
     }
 }
